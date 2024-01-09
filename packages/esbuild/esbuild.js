c387b1d3c94bd56e0f0327e1cc1fb45d16040c1a
fourdim <59462000+fourdim@users.noreply.github.com>
2024-01-09 05:01:36 -0500

    fix: esbuild does not support top level await (#29)

--- packages/esbuild/esbuild.js
+++ packages/esbuild/esbuild.js
@@ -1,12 +1,11 @@
 import { build } from 'esbuild'
-import { polyfillNode } from 'esbuild-plugin-polyfill-node'
 import htmlPlugin from '@chialab/esbuild-plugin-html'
 
 await build({
   bundle: true,
   entryPoints: ['src/index.html'],
   outdir: 'dist',
-  plugins: [polyfillNode(), htmlPlugin()],
+  plugins: [htmlPlugin()],
   treeShaking: true,
   define: {
     'process.stdout.isTTY': 'false',
